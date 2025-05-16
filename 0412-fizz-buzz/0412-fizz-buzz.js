65ee6c30d989508890082d19d1b6c9174f90aa22
iamfaisalpk <iamfaizy123@gmail.com>
2025-05-16 15:56:22 +0530

    Time: 2 ms (42.8%), Space: 58.7 MB (18.64%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    var res =[]
+
+    for(let i= 1; i <=n; i++){
+        let string= ''
+        if (i % 3 === 0) string += 'Fizz'
+        if (i % 5 === 0) string += 'Buzz'
+
+        if (string === '') string += i
+        res.push(string)
+    }
+
+    return res
+};
